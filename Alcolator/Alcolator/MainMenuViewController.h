74ba85f4455c66256e5d6cb91e09aee49d1776df
Xoi Hin <xoihin@yahoo.com>
2015-08-19 11:13:16 -0400

    Checkpoint 21 completed, basic navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Xoi's iMac on 2015-08-19.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
